c2d79adc019ef13c487ab5c9e8b1b279de8ab199
David Whiteman <dlwhiteman@icloud.com>
2024-08-17 16:41:07 -0400

    improved FAQ and other things
    - changed FAQ page to allow answers to be individually displayed and formatted nicely
    - fixed color issues with inputs
    - improved wording and spacing in different places
    - made multiselect have similar border style as inputs
    - change signup link on about page to go to /findTutor
    - fix layout of nav bar on mobile devices

--- src/App.vue
+++ src/App.vue
@@ -31,23 +31,29 @@ const signOut = () => {
 <style scoped>
 .routers {
   display: flex;
+  flex-wrap: wrap;
   gap: 0.5rem;
 }
+
 .router {
   flex: 1;
 }
+
 .grow-2 {
   flex-grow: 2;
 }
+
 .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
