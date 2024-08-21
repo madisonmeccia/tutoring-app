41014e84db18c4e48014d3742fd471e6404e3a16
David Whiteman <dlwhiteman@icloud.com>
2024-08-20 21:01:29 -0400

    fix: better + and - icons for faq
    - improved the + and - icons for in FAQ for better alignment
    - made the nav bar text responsively smaller on mobile
    - fix color of text selection to be easier to see against dark background

--- src/components/FaqComponent.vue
+++ src/components/FaqComponent.vue
@@ -112,14 +112,14 @@ const toggleElement = (id) => {
       <p style="cursor: pointer">
         <div @click="toggleElement('q' + groupIndex + questionIndex)" class="flex flex-row">
           <!-- This is the plus image -->
-          <svg :id="'q' + groupIndex + questionIndex + 'plus'" class="block size-6" xmlns="http://www.w3.org/2000/svg"
+          <svg :id="'q' + groupIndex + questionIndex + 'plus'" class="block min-w-6 size-6" xmlns="http://www.w3.org/2000/svg"
             width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
             stroke-linecap="round" stroke-linejoin="round">
             <path d="M5 12h14"></path>
             <path d="M12 5v14"></path>
           </svg>
           <!-- This is the minus image -->
-          <svg :id="'q' + groupIndex + questionIndex + 'minus'" class="hidden size-6" xmlns="http://www.w3.org/2000/svg"
+          <svg :id="'q' + groupIndex + questionIndex + 'minus'" class="hidden min-w-6 size-6" xmlns="http://www.w3.org/2000/svg"
             width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
             stroke-linecap="round" stroke-linejoin="round">
             <path d="M5 12h14"></path>
